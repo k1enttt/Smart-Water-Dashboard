e48d2c1e1b2a12d436ba6773fd93399127ae3c44
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-06-12 16:27:42 +0700

    use local chromium instead of remote one

--- app/api/weather-data/route.ts
+++ app/api/weather-data/route.ts
@@ -15,9 +15,6 @@ export async function GET() {
       browser = await puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(
-          "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
-        ),
         headless: chromium.headless,
       });
     }
